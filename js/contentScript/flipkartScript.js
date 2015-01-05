47a2da4978cbb61dcfab7fedc7adfe587d9c2c2c
gauravroxs <gaurav.the.iiitian@gmail.com>
2015-01-05 15:38:05 +0530

    angularjs usage

--- js/contentScript/flipkartScript.js
+++ js/contentScript/flipkartScript.js
@@ -7,18 +7,6 @@ function addToStore() {
 	
 	var data = {priceInfo: allPrices, details: details};
 	storage.addToCollection('priceDropFlipkartData', url, data);
-//	chrome.storage.sync.get('priceDropFlipkartData', function (data) {
-//		
-//		if(data['priceDropFlipkartData'] != null && data['priceDropFlipkartData'] != undefined) {
-//			var urlArray = data['priceDropFlipkartData'];
-//			if(urlArray.indexOf(url) == -1) {
-//				urlArray.push(url);
-//				chrome.storage.sync.set({'priceDropFlipkartData': urlArray});
-//			}
-//		} else {
-//			chrome.storage.sync.set({'priceDropFlipkartData': [url]});
-//		}
-//	});
 }
 
 var form = document.getElementsByName("express-checkout-form");
--- js/controller/popupController.js
+++ js/controller/popupController.js
@@ -1,52 +1,55 @@
-document.addEventListener('DOMContentLoaded', function () {
-		chrome.storage.sync.get('priceDropFlipkartData', function (data) {
-			console.log(data);
-			var map = data['priceDropFlipkartData'];
-			for(var key in map) {
-				if(map.hasOwnProperty(key)) {
-					flipkart.successCallback(map[key], null, null);
-				}
-			}
-//			var links = data['priceDropFlipkartData'];
-//			for(var i=0; i<links.length; i++) {
-//				$.ajax({
-//					url: links[i],
-//					success: flipkart.successCallback,
-//					error: function (jqXHR, textStatus, errorThrown) {
-//						console.log(textStatus);
-//						console.log(errorThrown);
-//					}
-//				});
-//			}
+var priceDropApp = angular.module('priceDropApp', []);
+
+priceDropApp.factory('storageService', function() {
+	  return storage;
+});
+
+priceDropApp.controller('PopupController',['$scope', 'storageService', function ($scope, storageService) {
+	storageService.getCollection('priceDropFlipkartData', function(data) {
+		$scope.$apply(function () {
+			$scope.data = data;
 		});
 	});
+}]);
 
-var flipkart = {
-		successCallback : function success(data, textStatus, jqXHR) {
-				
-				var allPrices = new PriceInfo();
-				allPrices.mainPrice = data.priceInfo.mainPrice;
-				allPrices.exchangePrice = data.priceInfo.exchangePrice;
-				allPrices.otherPrices = data.priceInfo.otherPrices;
-				
-				var details = data.details;
-				$("#flipkart-list")[0].innerHTML += details.name + "<br>";
-
-				if(!allPrices.isNull()) {
-					if(allPrices.exchangePrice != null) {
-						$("#flipkart-list")[0].innerHTML += "Original Price: " + allPrices.mainPrice + "<br />";
-						$("#flipkart-list")[0].innerHTML += "Exchange Price: " + allPrices.exchangePrice + "<br />";
-					} else {
-						$("#flipkart-list")[0].innerHTML += "Main Price: " + allPrices.mainPrice + "<br />";
-					}
-					
-					if(allPrices.otherPrices.length != 0) {
-						$("#flipkart-list")[0].innerHTML += "Other Prices : <br/>";
-						for(var i=0; i<allPrices.otherPrices.length; i++) {
-							$("#flipkart-list")[0].innerHTML += allPrices.otherPrices[i] + "<br />";
-						}
-					}
-				}
-				$("#flipkart-list")[0].innerHTML += '-------------------------------------------------<br>';
-			}
-};
+//document.addEventListener('DOMContentLoaded', function () {
+//		chrome.storage.sync.get('priceDropFlipkartData', function (data) {
+//			console.log(data);
+//			var map = data['priceDropFlipkartData'];
+//			for(var key in map) {
+//				if(map.hasOwnProperty(key)) {
+//					flipkart.successCallback(map[key], null, null);
+//				}
+//			}
+//		});
+//	});
+//
+//var flipkart = {
+//		successCallback : function success(data, textStatus, jqXHR) {
+//				
+//				var allPrices = new PriceInfo();
+//				allPrices.mainPrice = data.priceInfo.mainPrice;
+//				allPrices.exchangePrice = data.priceInfo.exchangePrice;
+//				allPrices.otherPrices = data.priceInfo.otherPrices;
+//				
+//				var details = data.details;
+//				$("#flipkart-list")[0].innerHTML += details.name + "<br>";
+//
+//				if(!allPrices.isNull()) {
+//					if(allPrices.exchangePrice != null) {
+//						$("#flipkart-list")[0].innerHTML += "Original Price: " + allPrices.mainPrice + "<br />";
+//						$("#flipkart-list")[0].innerHTML += "Exchange Price: " + allPrices.exchangePrice + "<br />";
+//					} else {
+//						$("#flipkart-list")[0].innerHTML += "Main Price: " + allPrices.mainPrice + "<br />";
+//					}
+//					
+//					if(allPrices.otherPrices.length != 0) {
+//						$("#flipkart-list")[0].innerHTML += "Other Prices : <br/>";
+//						for(var i=0; i<allPrices.otherPrices.length; i++) {
+//							$("#flipkart-list")[0].innerHTML += allPrices.otherPrices[i] + "<br />";
+//						}
+//					}
+//				}
+//				$("#flipkart-list")[0].innerHTML += '-------------------------------------------------<br>';
+//			}
+//};
