0a4a41f53176548b915871878333d209ccca56c5
gauravroxs <gaurav.the.iiitian@gmail.com>
2015-01-14 15:14:13 +0530

    contentScript changes

--- js/contentScript/flipkartScript.js
+++ js/contentScript/flipkartScript.js
@@ -12,10 +12,14 @@ function addToStore() {
 	storage.addToCollection('priceDropFlipkartData', url, data);
 }
 
-var form = document.getElementsByName("express-checkout-form");
+var form = document.getElementsByName("buy-now-form");
+var lineBreak = document.createElement("BR");
+var lineBreak1 = document.createElement("BR");
 var input = document.createElement("INPUT");
 input.type = "button";
 input.value =  "Add to PriceDrop";
-input.className += " btn-express-checkout btn-big current";
+input.className += " btn-buy-now btn-big current";
 input.onclick = addToStore;
+form[0].appendChild(lineBreak);
+form[0].appendChild(lineBreak1);
 form[0].appendChild(input);
